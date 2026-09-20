262ca50d3e135d0b26f49ff0ae896b4eaeb1aea2
lfxxxqwq <3515490755@qq.com>
2026-09-20 11:54:06 +0800

    第一次作业

--- level1/p01_running_letter/main.cpp
+++ level1/p01_running_letter/main.cpp
@@ -1,20 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <windows.h>
 using namespace std;
-const int N = 10, SLEEP = 50;
+const int SLEEP = 50;
+int N;
+void Getlength()
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(hOut, &info);
+	N = info.srWindow.Right - info.srWindow.Left + 1;
+}
 int main()
 {
-	for (int i = 1; i <= N; ++i) {
-		for (int j = 1; j < i; ++j) cout << " ";
-		cout << "a";
-		Sleep(SLEEP);
-		system("cls");
-	}
-	for (int i = N; i >= 1; --i) {
-		for (int j = 1; j < i; ++j) cout << " ";
-		cout << "a";
-		Sleep(SLEEP);
-		system("cls");
+	Getlength();
+	while (true) {
+		for (int i = 1; i <= N; ++i) {
+			for (int j = 1; j < i; ++j) cout << " ";
+			cout << "a";
+			Sleep(SLEEP);
+			system("cls");
+		}
+		for (int i = N; i >= 1; --i) {
+			for (int j = 1; j < i; ++j) cout << " ";
+			cout << "a";
+			Sleep(SLEEP);
+			system("cls");
+		}
 	}
 	return 0;
 }
